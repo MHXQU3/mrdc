00d5f16a8a602dbe5f170b48727ed5acde84e75d
Minhaz Hoque <mi8ho7455@gmail.com>
2024-07-28 15:54:43 +0100

    read YAML file code

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,19 @@
 class DatabaseConnector:
-    pass
+
+    def __init__(self, path):
+        self.path = path
+    
+    def read_db_creds(self):
+        dict = {}
+        with open(path, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line:
+                    key, value = line.split(':', 1)
+                    dict[key.strip()] = value.strip()
+        print(dict)
+
+path = 'db_creds.yaml'
+connector = DatabaseConnector(path)
+connector.read_db_creds()
+    
