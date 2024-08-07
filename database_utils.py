1fcc330be554960a706e946767ea3f8991e99da8
Minhaz Hoque <mi8ho7455@gmail.com>
2024-08-07 22:18:46 +0100

    Updated file

--- database_utils.py
+++ database_utils.py
@@ -1,17 +1,29 @@
+from sqlalchemy import create_engine
+import yaml 
+
 class DatabaseConnector:
 
     def __init__(self, path):
         self.path = path
     
     def read_db_creds(self):
-        dict = {}
-        with open(path, 'r') as file:
-            for line in file:
-                line = line.strip()
-                if line:
-                    key, value = line.split(':', 1)
-                    dict[key.strip()] = value.strip()
-        print(dict)
+        with open(self.path, 'r') as file:
+            db_creds = yaml.safe_load(file)
+        return db_creds
+    
+    def init_db_engine(self):
+        db_creds = self.read_db_creds()
+        HOST = db_creds.get('RDS_HOST')
+        PASSWORD = db_creds.get('RDS_PASSWORD')
+        USER = db_creds.get('RDS_USER')
+        DATABASE = db_creds.get('RDS_DATABASE')
+        PORT = db_creds.get('RDS_PORT')
+        DBTYPE = db_creds.get('RDS_DATABASE_TYPE')
+        DBAPI = db_creds.get('DBAPI')
+
+        url = f'{DBTYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
+        engine = create_engine(url)
+        return engine
 
 path = 'db_creds.yaml'
 connector = DatabaseConnector(path)
