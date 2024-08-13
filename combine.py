54281c7b8412422b128e3a9e7b6d2b03a2f5ee57
Minhaz Hoque <mi8ho7455@gmail.com>
2024-08-13 15:17:24 +0100

    Updated files

--- combine.py
+++ combine.py
@@ -11,8 +11,8 @@ def main():
     user_data_df = extractor.extract_user_data('legacy_users')
     
     # Clean the extracted user data
-    cleaner = DataCleaning()
-    cleaned_data_df = cleaner.clean_user_data(user_data_df)
+    clean = DataCleaning()
+    cleaned_data_df = clean.clean_user_data(user_data_df)
     
     # Upload the cleaned data to the 'dim_users' table in the 'sales_data' database
     connector.upload_to_db(cleaned_data_df, 'dim_users', connector.read_db_creds())
--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, inspect
 import yaml 
 import pandas as pd
+import tabula as tb
 import psycopg2
 from database_utils import DatabaseConnector
 
@@ -17,9 +18,17 @@ class DataExtractor:
     def extract_user_data(self, table_name):
         return self.read_rds_table(table_name)
     
+    def retrieve_pdf_data(self, link):
+        pdf_data = tb.read_pdf(link, pages='all', stream=True)
+        return pdf_data
+    
 connector = DatabaseConnector('db_creds.yaml')
 extractor = DataExtractor(connector)
 
 # Extract user data
 user_data_df = extractor.extract_user_data('legacy_users')
 print(user_data_df.head()) 
+
+link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
+pdf_df = extractor.retrieve_pdf_data(link)
+print(pdf_df.head())
