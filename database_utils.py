06d8360b17f449f3b72386858fefa94cff2cdf35
Minhaz Hoque <mi8ho7455@gmail.com>
2024-09-20 23:01:05 +0100

    Finish comments

--- database_utils.py
+++ database_utils.py
@@ -10,20 +10,20 @@ class DatabaseConnector:
         pass
 
     def read_db_creds(self):
-        with open("db_creds.yaml", "r") as f:
+        with open("db_creds.yaml", "r") as f: # Open and read the database credentials from a YAML file
             db_creds = yaml.safe_load(f)
-        return db_creds #Take in yaml and return a dict
+        return db_creds #Take in YAML and return a dict
 
     def init_db_engine(self, db_creds):
         engine = create_engine(f"{db_creds['RDS_DATABASE_TYPE']}+{db_creds['DB_API']}://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
         engine.connect()
         
-        # Take in the db_creds output and initialise and return an sql_alchemy database engine
+        # Take in the db_creds output and initialise and return an sqlalchemy database engine
         return engine
 
     def list_db_tables(self, engine):
         engine.connect()
-        inspector = inspect(engine)
+        inspector = inspect(engine) # Create an inspector object to retrieve database schema information
         table_names = inspector.get_table_names()
         return table_names
         # Using the engine from init_db_engine() list all the tables in the database so you know whhich tables you can extract data from
@@ -31,4 +31,4 @@ class DatabaseConnector:
     def upload_to_db(self, data_frame, table_name, db_creds):
         local_engine = create_engine(f"{db_creds['LOCAL_DATABASE_TYPE']}+{db_creds['LOCAL_DB_API']}://{db_creds['LOCAL_USER']}:{db_creds['LOCAL_PASSWORD']}@{db_creds['LOCAL_HOST']}:{db_creds['LOCAL_PORT']}/{db_creds['LOCAL_DATABASE']}")
         local_engine.connect()
-        data_frame.to_sql(table_name, local_engine, if_exists='replace')
+        data_frame.to_sql(table_name, local_engine, if_exists='replace') # Upload the DataFrame to the specified table in the local database, replacing if it already exists
